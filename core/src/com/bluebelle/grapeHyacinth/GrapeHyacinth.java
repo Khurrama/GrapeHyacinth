edec7ae00300cb501638fb3896ee7d511e0afe18
Khurrama <Andrew.vanardennen@gmail.com>
2014-07-01 22:14:21 +0200

    Alpha Test - V0.0.0.000.0001

---
+++ core/src/com/bluebelle/grapeHyacinth/GrapeHyacinth.java
@@ -0,0 +1,46 @@
+package com.bluebelle.grapeHyacinth;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+
+public class GrapeHyacinth extends ApplicationAdapter {
+	private SpriteBatch batch;
+//	private BitmapFont font;
+	private FreeTypeFontGenerator generator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+    private BitmapFont font12;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	    //font = new BitmapFont();
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/segoeui.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parameter.size = 64;
+        font12 = generator.generateFont(parameter); //font size 12 pixels
+        font12.setColor(Color.BLACK);
+        generator.dispose();
+    }
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+        font12.draw(batch, "Hello world, again!", 200,200);
+		batch.end();
+	}
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ desktop/src/com/bluebelle/grapeHyacinth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bluebelle.grapeHyacinth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bluebelle.grapeHyacinth.GrapeHyacinth;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GrapeHyacinth(), config);
+	}
+}
